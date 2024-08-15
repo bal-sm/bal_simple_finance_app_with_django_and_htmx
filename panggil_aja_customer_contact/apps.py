00ae4c483519aeef2515f4a4a6b8aac135cf4dd3
Mahmuda <bal.mahmuda@gmail.com>
2024-08-15 15:06:49 +0700

    Started `panggil_aja_customer_contact` app

---
+++ panggil_aja_customer_contact/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PanggilAjaCustomerContactConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'panggil_aja_customer_contact'
