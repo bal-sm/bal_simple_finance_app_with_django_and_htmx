455c75f0d0f02a27e6e4a2d1cac2e4f09703f71f
Mahmuda <bal.mahmuda@gmail.com>
2024-08-15 15:24:55 +0700

    Created `ContactForm`

---
+++ panggil_aja_customer_contact/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from panggil_aja_customer_contact.models import Contact
+
+
+class ContactForm(forms.Form):
+    class Meta:
+        model = Contact
+        fields = ["name", "phone_number"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "phone_number": forms.TextInput(attrs={"class": "form-control"}),
+        }
