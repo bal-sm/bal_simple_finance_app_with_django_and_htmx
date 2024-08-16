823041f483dab57a6137a66c6a8cdaf8218aa92a
Mahmuda <bal.mahmuda@gmail.com>
2024-08-16 11:37:01 +0700

    Modified to `ModelForm`
    (rada sic).
    
    > oops salah

--- panggil_aja_customer_contact/forms.py
+++ panggil_aja_customer_contact/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from panggil_aja_customer_contact.models import Contact
 
 
-class ContactForm(forms.Form):
+class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ["name", "phone_number"]
