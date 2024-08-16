7072432b42ddf2ae4c7bd51f2e22f26453841209
Mahmuda <bal.mahmuda@gmail.com>
2024-08-16 11:21:46 +0700

    Created `index` view
    and rendered the `form` from `context` to template.

--- panggil_aja_customer_contact/views.py
+++ panggil_aja_customer_contact/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from .models import Contact
 
 # Create your views here.
+
+
+def index(request):
+    context = {"form": ContactForm()}
+    return render(request, "index.html", context)
